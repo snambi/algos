3b48b9374dd27e22f4af0c9728c18836769c6da6
Nambi Sankaran <snambi@gmail.com>
2025-04-23 18:01:58 -0700

    done

---
+++ src/main/java/org/github/code/algos/questions/Question1.java
@@ -0,0 +1,16 @@
+package org.github.code.algos.questions;
+
+import static org.github.code.algos.questions.Question1.Print.println;
+
+public class Question1 {
+
+    public static void main(String[] args ){
+        println("Hello World");
+    }
+
+    public static class Print{
+        public static void println(String msg){
+            System.out.println(msg);
+        }
+    }
+}
